7e698b99dbcbc372f1111158e271f01d520377f9
KimBH04 <kimbh0437@gmail.com>
2024-01-09 22:01:50 +0900

    닉네임 표시

--- Suika99/Assets/1.Scripts/Managers/GameManager.cs
+++ Suika99/Assets/1.Scripts/Managers/GameManager.cs
@@ -3,13 +3,15 @@ using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
-public class GameManager : MonoBehaviourPunCallbacks
+public class GameManager : MonoBehaviourPunCallbacks, IPunObservable
 {
     public static GameManager Instance { get; private set; }
 
-    public GameObject[] anotherBaskets;
+    [SerializeField] private GameObject[] anotherBaskets;
+    [SerializeField] private TMP_Text[] playerNames;
 
     private Room room;
 
@@ -27,7 +29,6 @@ public class GameManager : MonoBehaviourPunCallbacks
         set
         {
             isEnd = value;
-            Debug.Log("Game Set");
         }
     }
 
@@ -38,28 +39,61 @@ public class GameManager : MonoBehaviourPunCallbacks
 
         room = PhotonNetwork.CurrentRoom;
 
-        userIndex = room.PlayerCount;
-        int playerCount = userIndex - 1;
+        userIndex = room.PlayerCount - 1;
+        int playerCount = userIndex;
         for (int i = 0; i < playerCount; i++)
         {
             anotherBaskets[i].SetActive(true);
         }
 
-        int index = 1;
-        foreach (var item in room.Players.OrderBy(x => x.Key).Select(x => x.Value))
+        int index = 0;
+        foreach (var nickName in room.Players.OrderBy(x => x.Key).Select(x => x.Value.NickName))
         {
-            Debug.Log(item.ToString());
-            playerIndexes.Add(item.NickName, index++);
+            if (nickName != PhotonNetwork.NickName)
+            {
+                playerNames[index].text = nickName;
+                playerNames[index].gameObject.SetActive(true);
+
+                playerIndexes.Add(nickName, index++);
+            }
         }
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
-        anotherBaskets[room.PlayerCount - 2].SetActive(true);
+        int newPlayersIndex = room.PlayerCount - 2;
+        playerIndexes.Add(newPlayer.NickName, newPlayersIndex);
+
+        playerNames[newPlayersIndex].text = newPlayer.NickName;
+        anotherBaskets[newPlayersIndex].SetActive(true);
+        playerNames[newPlayersIndex].gameObject.SetActive(true);
     }
 
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
         anotherBaskets[room.PlayerCount - 1].SetActive(false);
+        playerNames[room.PlayerCount - 1].gameObject.SetActive(false);
+
+        foreach (var nickName in playerIndexes.Keys.ToArray())
+        {
+            if (playerIndexes[nickName] > playerIndexes[otherPlayer.NickName])
+            {
+                int index = --playerIndexes[nickName];
+                playerNames[index].text = nickName;
+            }
+            Debug.Log($"{nickName} {playerIndexes[nickName]}");
+        }
+
+        if (userIndex > playerIndexes[otherPlayer.NickName])
+        {
+            userIndex--;
+        }
+
+        playerIndexes.Remove(otherPlayer.NickName);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+
     }
 }
